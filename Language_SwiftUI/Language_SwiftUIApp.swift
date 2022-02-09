040c88be9431ae7eb9f1e843bb1a3ab755633338
Sebastian <s.kloesel@gmx.de>
2022-02-09 19:04:54 +0100

    Initial Commit

---
+++ Language_SwiftUI/Language_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Language_SwiftUIApp.swift
+//  Language_SwiftUI
+//
+//  Created by Sebastian Klösel on 09.02.22.
+//
+
+import SwiftUI
+
+@main
+struct Language_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
